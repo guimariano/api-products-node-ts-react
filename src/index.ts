337700ec35cbeb1533d9cb8875eceef2306e41c3
guimariano <guilherme.mariano.ext@infracommerce.com.br>
2023-07-02 22:28:02 -0300

    fix: conserta problemas em produção

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { Knex as knex } from '@server/database/knex';
 import { server } from '@server/server';
 
 const port: string | undefined = process.env.PORT;
 
-server.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+const startServer = () => {
+  server.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${ port }`);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  knex.migrate.latest()
+    .then(() => startServer())
+    .catch(console.log);
+} else {
+  startServer();
+}
+
